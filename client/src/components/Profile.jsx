0bc72d725d5443b2731eef10277e9ff2e3011427
SmaouiAli <smaouiali22@gmail.com>
2020-10-16 09:41:03 +0100

    added a home button && and a check step buttons && altered the tutorial videos width+height and centered them

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,82 +1,107 @@
-import React, { Component } from 'react';
-import MongoDB from './MongoDB.jsx'
-import NodeJs from './NodeJs.jsx'
-import ExpressJs from './ExpressJs.jsx'
+import React, { Component } from "react";
+import MongoDB from "./MongoDB.jsx";
+import NodeJs from "./NodeJs.jsx";
+import ExpressJs from "./ExpressJs.jsx";
 export default class Profile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            check:''
-        }
-        this.renderExpressTut = this.renderExpressTut.bind(this);
-        this.renderMongoTut = this.renderMongoTut.bind(this);
-        this.renderNodeTut = this.renderNodeTut.bind(this);
-    }
-renderNodeTut(event){
-event.preventDefault();
-if(this.state.check===''||this.state.check==='Mongo'||this.state.check==='Express'){
-this.setState({check:'Node'})}
-}
-renderMongoTut(event){
+  constructor(props) {
+    super(props);
+    this.state = {
+      check: "",
+    };
+    this.renderExpressTut = this.renderExpressTut.bind(this);
+    this.renderMongoTut = this.renderMongoTut.bind(this);
+    this.renderNodeTut = this.renderNodeTut.bind(this);
+    this.backHome= this.backHome.bind(this);
+  }
+  renderNodeTut(event) {
     event.preventDefault();
-    if(this.state.check===''||this.state.check==='Node'||this.state.check==='Express'){
-    this.setState({check:'Mongo'})
-    }}
-    renderExpressTut(event){
-        event.preventDefault();
-        if(this.state.check===''||this.state.check==='Mongo'||this.state.check==='Node'){
-        this.setState({check:'Express'})
-        }}
-
-    render() {
-        if(this.state.check === ''){
-        return (
-            <div>
-                <div className="navbar">
-                    <span className="navbar" onClick={this.renderNodeTut}>NodeJs</span>
-                    <span className="navbar" onClick={this.renderExpressTut}>ExpressJs</span>
-                    <span className="navbar" onClick={this.renderMongoTut}>MongoDB</span>
-                    <span className="navbar" >Score</span>
-                </div>
-                <p> Chooose from the courses above</p>
-                
-            </div>
-        ) } else if(this.state.check === 'Node'){
-            return (
-                <div>
-                    <div className="navbar">
-                        <span className="navbar" onClick={this.renderNodeTut}>NodeJs</span>
-                        <span className="navbar" >ExpressJs</span>
-                        <span className="navbar" >MongoDB</span>
-                        <span className="navbar" >Score</span>
-                    </div>
-                    <NodeJs/>
-                    
-                </div>
-             ) }else if(this.state.check === 'Mongo'){
-                return (
-                    <div>
-                        <div className="navbar">
-                            <span className="navbar" >NodeJs</span>
-                            <span className="navbar" >ExpressJs</span>
-                            <span className="navbar"onClick={this.renderMongoTut} >MongoDB</span>
-                            <span className="navbar" >Score</span>
-                        </div>
-                        <MongoDB/>
-                        
-                    </div>
-                 ) }else if(this.state.check === 'Express'){
-                    return (
-                        <div>
-                            <div className="navbar">
-                                <span className="navbar" >NodeJs</span>
-                                <span className="navbar" onClick={this.renderExpressTut}>ExpressJs</span>
-                                <span className="navbar" >MongoDB</span>
-                                <span className="navbar" >Score</span>
-                            </div>
-                            <ExpressJs/>
-                            
-                        </div>
-                     ) }
+    if (
+      this.state.check === "" ||
+      this.state.check === "Mongo" ||
+      this.state.check === "Express"
+    ) {
+      this.setState({ check: "Node" });
     }
+  }
+  renderMongoTut(event) {
+    event.preventDefault();
+    if (
+      this.state.check === "" ||
+      this.state.check === "Node" ||
+      this.state.check === "Express"
+    ) {
+      this.setState({ check: "Mongo" });
+    }
+  }
+  renderExpressTut(event) {
+    event.preventDefault();
+    if (
+      this.state.check === "" ||
+      this.state.check === "Mongo" ||
+      this.state.check === "Node"
+    ) {
+      this.setState({ check: "Express" });
+    }
+  }
+  backHome(event){
+    event.preventDefault();
+    this.setState({check:''})
+  }
+
+  render() {
+    if (this.state.check === "") {
+      return (
+        <div>
+          <div>
+            <span onClick={this.renderNodeTut}>NodeJs</span>
+            <span onClick={this.renderExpressTut}>ExpressJs</span>
+            <span onClick={this.renderMongoTut}>MongoDB</span>
+            <span>Score</span>
+            <span onClick={this.backHome}>Home</span>
+          </div>
+      <h3>welcome {this.props.name}</h3>
+          <p> Chooose from the courses above</p>
+        </div>
+      );
+    } else if (this.state.check === "Node") {
+      return (
+        <div>
+          <div>
+            <span onClick={this.renderNodeTut}>NodeJs</span>
+            <span>ExpressJs</span>
+            <span>MongoDB</span>
+            <span>Score</span>
+            <span onClick={this.backHome}>Home</span>
+          </div>
+          <NodeJs />
+        </div>
+      );
+    } else if (this.state.check === "Mongo") {
+      return (
+        <div>
+          <div>
+            <span>NodeJs</span>
+            <span>ExpressJs</span>
+            <span onClick={this.renderMongoTut}>MongoDB</span>
+            <span>Score</span>
+            <span onClick={this.backHome}>Home</span>
+          </div>
+          <MongoDB />
+        </div>
+      );
+    } else if (this.state.check === "Express") {
+      return (
+        <div>
+          <div>
+            <span>NodeJs</span>
+            <span onClick={this.renderExpressTut}>ExpressJs</span>
+            <span>MongoDB</span>
+            <span >Score</span>
+            <span onClick={this.backHome}>Home</span>
+          </div>
+          <ExpressJs />
+        </div>
+      );
+    }
+  }
 }
